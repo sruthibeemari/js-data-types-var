7d0d572bea2edec79465190d300d7eeccf725437
Your Name <you@example.com>
2024-09-18 21:42:47 +0530

    added all files

---
+++ scirpt.js
@@ -0,0 +1,39 @@
+//Object
+var details={
+   name:"sruthi",
+   age:20,
+
+
+}
+
+
+console.log(details);
+
+var mother={
+   name:"srilekha",
+   age:39,
+   occuptn:"housewife",
+   child:{
+    name1:"sruthi",
+    age:20,
+    occupation:"student"
+}
+  }
+  console.log(mother);
+  //array
+  var cars=[bmw=[ "bmw","blue",266300], audi=[ "audi" ,"white",366600]]
+  console.log(cars);
+//boolean
+  var boolean={
+   age:20,
+  
+  }
+  if (boolean.age = 20) {
+   console.log(true);
+  }
+  else{
+   console.log(false);
+   
+  }
+
+
